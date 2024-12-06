7f9820bbe40a27abf64541efce38315f4cc1e232
Rancy21 <rancyontsandaga@gmail.com>
2024-12-06 18:54:45 +0200

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import LoginPage from "./css/Login.jsx";
+import LoginPage from "./pages/Login.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LoginPage />
+    <Login />
   </StrictMode>
 );
