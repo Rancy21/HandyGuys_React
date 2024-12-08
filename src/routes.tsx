e6bda8352b2bcafa1eacf2d2922060ccabcbca84
Rancy21 <rancyontsandaga@gmail.com>
2024-12-08 15:13:50 +0200

    Created Profile

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter } from "react-router";
 import Login from "../src/pages/Login.jsx";
 import Home from "./Pages/Home.jsx";
+import Profile from "./Pages/Profile.jsx";
 import Signup from "./pages/SignUp.jsx";
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
   {
     path: "/sign",
     element: <Signup />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
 ]);
 
