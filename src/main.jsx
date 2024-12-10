9611a8bdd07abb04398e5ac0a387bc0d474577cf
Rancy21 <rancyontsandaga@gmail.com>
2024-12-10 17:18:05 +0200

    added redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 import { RouterProvider } from "react-router";
+import { Provider } from "react-redux";
 import router from "./routes.tsx";
+import { store } from "./store/store.jsx";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
 );
