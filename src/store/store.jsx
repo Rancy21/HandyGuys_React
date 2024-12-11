b91a3cf373efa2921e60e585f37191a67e5b68bb
Rancy21 <rancyontsandaga@gmail.com>
2024-12-11 16:14:54 +0200

    added persistore in the store

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import userReducer from "./userSlice";
+
+const persistConfig = {
+  key: "root", // key is required
+  version: 1,
+  storage // define which storage to use
+  // Optionally, you can blacklist or whitelist specific reducers
+  // blacklist: ['someReducer']
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
-  }
+    user: persistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+export const persistor = persistStore(store);
